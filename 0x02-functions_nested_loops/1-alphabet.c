2e89792b3854b46f09cdcdb4ad3c76ff88cc02f3
ImmaculataGregory <boldtaste_official@yahoo.com>
2023-07-19 09:20:44 -0700

    ./2-alphabet_x10

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,7 +7,9 @@ void print_alphabet_x10(void)
 {
 char i, k;
 for (k = 0; k < 10; k++)
-for (i = 'a' ; i <= 'z' ; i++)
+for (i = 'a' ; )
+for (i <= 'z' ; )
+for (i++)
 _putchar(i);
 _putchar('\n');
 }
