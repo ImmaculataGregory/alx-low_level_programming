1fb3eb30d07660b1b3b1136446d8df4c7f5c5ee9
ImmaculataGregory <boldtaste_official@yahoo.com>
2023-07-21 06:06:22 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,12 +11,10 @@ int main(void)
 	int a, b;
 
 	for (a = 1; a < 1024; a++)
-	{
 		{
 		if ((a % 3) == 0 || (a % 5) == 0)
-			b += a;
+				b += a ;
 		}
-	printf("%d\n", b);
-	}
+	printf("%d\n, ", b);
 	return (0);
 }
