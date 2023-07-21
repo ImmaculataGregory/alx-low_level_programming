9d5392e31eb4f8c403e14df06b3d75ab604db2e5
ImmaculataGregory <boldtaste_official@yahoo.com>
2023-07-21 05:53:07 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,9 +11,11 @@ int main(void)
 
 	for (a = 1; a < 1024; a++)
 	{
-		if ((a % 3) == 0 || (a % 5) == 0
+		{
+		if ((a % 3) == 0 || (a % 5) == 0)
 			b += a;
-	}
+		}
 	printf("%d\n", b);
+	}
 	return (0);
 }
