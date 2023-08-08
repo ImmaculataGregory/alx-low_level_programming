6bedf438e095fda80eba10468cb25ba3fee4d717
ImmaculataGregory <boldtaste_official@yahoo.com>
2023-08-08 03:43:53 -0700

    static libraries

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# compile all .c files in the current directory into object files
+gcc -c *.c
+
+# create the static library liball.a from all the generated objects files
+ar rcs liball.a *.o
+
+# clean up by removing the object files
+rm -f *.o
